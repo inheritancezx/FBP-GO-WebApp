09275d67b690e4d1ae2bbc5143295d0f3e151677
inheritancezx <fayzaaqilaar@gmail.com>
2024-11-10 22:35:24 +0700

    fix: save func

--- wiki.go
+++ wiki.go
@@ -67,6 +67,6 @@ func main() {
 	http.HandleFunc("/", handler)
 	http.HandleFunc("/view/", viewHandler)
 	http.HandleFunc("/edit/", editHandler)
-	// http.HandleFunc("/save/", saveHandler)
+	http.HandleFunc("/save/", saveHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
